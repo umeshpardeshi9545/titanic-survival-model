e0af9be4a815f4502f78c0d37b13fa3cb6b1d57e
Umesh  Pardeshi <121162178+umeshpardeshi9545@users.noreply.github.com>
2025-01-03 11:27:51 +0530

    newapp.py

--- newapp.py
+++ newapp.py
@@ -5,7 +5,7 @@ import pickle
 # URL to the raw model file on GitHub
 import urllib.request
 
-url = "https://raw.githubusercontent.com/username/repository/branch/Titanic_model.pkl"
+url = "https://github.com/umeshpardeshi9545/titanic-survival-model/blob/main/Titanic_model.pkl"
 with urllib.request.urlopen(url) as response:
     model = pickle.load(response)
 # Load the model with a corrected file path
